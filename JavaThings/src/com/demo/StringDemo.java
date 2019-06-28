08c0a2678cab0eccfa271b5c841eaf9e775f5f38
185720 <185720@USTEXPLNB512QN2.AMERICAS.GLOBAL.NTTDATA.COM>
2019-06-28 13:33:24 -0500

    This is the first commit

---
+++ JavaThings/src/com/demo/StringDemo.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		
+		String s1 = "Hello";
+		String s2 = "Wolrd";
+		
+		if(s1.equals(s2))
+		{
+			System.out.println("Identit"
+					+ "cal");
+		}
+		else
+			System.out.println("Not Identical");
+	}
+
+}
